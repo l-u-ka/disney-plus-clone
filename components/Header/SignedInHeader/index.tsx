d29a58fb3f864d87d2c9810a4434ed585a167ea3
Luka Kurtanidze <luka12208@gmail.com>
2024-05-30 20:40:21 +0400

    set up project and add header

---
+++ components/Header/SignedInHeader/index.tsx
@@ -0,0 +1,53 @@
+import { Session } from "next-auth";
+import React from "react";
+import { DropDown, NavMenu, SignOut, UserImg } from "../styles";
+import { signOut } from "next-auth/react";
+
+type Props = {
+  session: Session;
+};
+
+function SignedInHeader({ session }: Props) {
+  const image = session.user?.image as string;
+  const name = session.user?.name as string;
+  return (
+    <>
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="Home icon" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="Search icon" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="Watch List icon" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="Originals icon" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="Movie icon" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="Series icon" />
+          <span>Series</span>
+        </a>
+      </NavMenu>
+      <SignOut>
+        {session !==null ? (
+          <UserImg src={image} alt={name} />
+        ): null}
+        <DropDown>
+          <span onClick={()=>signOut()}>Sign Out</span>
+        </DropDown>
+      </SignOut>
+    </>
+  );
+}
+
+export default SignedInHeader;
---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider, type DefaultTheme } from "styled-components";
+import GlobalStyle from "../globalstyles";
+import Header from "@/components/Header";
+import { SessionProvider } from "next-auth/react";
+
+const theme: DefaultTheme = {
+  primary: "#040714",
+  secondary: "#0070f3",
+  white: "#fff",
+  brightBlue: "#3E64E5",
+  silver: "silver",
+  darkGrey: "#31343e",
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <SessionProvider session={pageProps.session}>
+          <GlobalStyle />
+          <Header/>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+      
+    </>
+  );
+}
