8148d7f248002dc2e229d25caedb496ab6236897
Luka Kurtanidze <luka12208@gmail.com>
2024-05-30 22:46:06 +0400

    add slider banner

--- components/HomeLayout/index.tsx
+++ components/HomeLayout/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import SliderBanner from './SliderBanner'
+import { Container } from './styles'
+import Categories from './Categories'
 
 type Props = {}
 
 function HomeLayout({}: Props) {
   return (
-    <div>HomeLayout</div>
+    <Container>
+      <SliderBanner/>
+      <Categories/>
+    </Container>
   )
 }
 
